07ac0fdd7fd8e9dffb289f929294035cb151547a
Vitor Barroso <vitorlauvresbarroso@gmail.com>
2024-08-27 13:18:35 -0300

    Adicionado hello world básico para o teste

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+axios.defaults.baseURL = process.env.VUE_APP_BASE_URL;
+
+const app = createApp(App);
+app.use(router)
+app.use(axios)
+app.config.globalProperties.$axios = axios
+app.use(VueAxios, axios)
+app.mount('#app');
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,29 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      apiReturn: "",
+      loader: true,
+    }
+  },
+  created() {
+    this.$axios.get('/hello_world')
+    .then(res => {
+      this.apiReturn = res.data.message;
+      this.loader = false;
+    })
   }
 }
 </script>
+
+<template>
+  <div class="home">
+    {{ loader ? 'Carregando...' : apiReturn }}
+  </div>
+</template>
+
+<style scoped>
+.home {
+  width: 100%;
+}
+</style>
